4101d967023957f80b113ae9ef47880dae77df9b
Yusuke Tanaka <y.tanaka@aktsk.jp>
2017-08-12 17:28:35 +0900

    First commit

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+	public Transform Player;
+	private Vector3 cameraOffset;
+
+	private void Start()
+	{
+		cameraOffset = GetComponent<Transform>().position - Player.position;
+	}
+
+	private void Update()
+	{
+		GetComponent<Transform>().position = Player.position + cameraOffset;
+	}
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public Rigidbody rb;
+
+	public void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	public void FixedUpdate()
+	{
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		rb.AddForce(x * 5, 0, z * 5);
+
+		if (rb.transform.position.y < 0.6 && Input.GetKeyDown(KeyCode.Space)) {
+			rb.AddForce(0, 3, 0, ForceMode.VelocityChange);
+		}
+
+		if (Input.GetKey(KeyCode.X)) {
+			rb.velocity = new Vector3(0, rb.velocity.y, 0);
+		}
+	}
+}
